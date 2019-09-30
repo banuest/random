806466908a391bd5f3cf5e2b06884bc52cca525a
Ben Nuest <binju821@gmail.com>
2019-09-29 20:28:41 -0500

    init

---
+++ lineterm.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Copyright (c) 2019 Ben Nuest
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+
+import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('filename', nargs='?', help='The filename to pull \
+        input from')
+parser.add_argument('-o', nargs='?', default=sys.stdout, dest='outfile', \
+        help='Filename to write to, default is stdout')
+args = parser.parse_args()
+
+# set variables equal to command-line options
+filename = args.filename
+outfile = args.outfile
+
+# open the file to be edited, read out by line, copy to 'lines' var
+with open(filename, 'r') as f:
+    lines = f.readlines()
+
+# remove the last character - the line terminator - from each line
+for line in lines:
+    line = line[:-1]
+
+# write the output to a new file, keeping the same filename and appending 
+# '.term' to the end
+if outfile == sys.stdout:
+    for line in lines:
+        sys.stdout.write(line)
+else:
+    with open(outfile, 'w') as f:
+        for line in lines:
+            f.write(line)
